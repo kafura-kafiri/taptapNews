ea8f9cb40d4b57172cc90d979a0be096699fd7f9
Mohammad <mohamad.kav@gmail.com>
2016-07-16 00:03:19 +0430

    Http starting to fix

--- sambalNews/static/taptapNews/js/controllers/LoginController.js
+++ sambalNews/static/taptapNews/js/controllers/LoginController.js
@@ -1,6 +1,6 @@
 
 
- app.controller('LoginController', ['$scope', function($scope, $http) {
+ app.controller('LoginController', ['$scope','$http', function($scope,$http) {
           $scope.login = function (username,password) {
             var data = $.param(
                 {
@@ -17,9 +17,37 @@
                 }
             }
 
-            $http.post('http://localhost:8000/sambalNews/login', data, config)
-            .success(function (data, status, headers, config) {
-                $scope.ResponseDetails = data;
+              $http.post('/sambalNews/login/', data, config)
+                .success(function (data, status, headers, config) {
+                    $scope.ResponseDetails = data;
+            })
+            .error(function (data, status, header, config) {
+                $scope.ResponseDetails = "Data: " + data +
+                    "<hr />status: " + status +
+                    "<hr />headers: " + header +
+                    "<hr />config: " + config;
+            });
+        };
+     $scope.signUp = function (username,password,email) {
+            var data = $.param(
+                {
+                    data: {
+                        username: username,
+                        password: password,
+                        email: email
+                    }
+                }
+            );
+
+            var config = {
+                headers : {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            }
+
+              $http.post('/sambalNews/signup', data, config)
+                .success(function (data, status, headers, config) {
+                    $scope.ResponseDetails = data;
             })
             .error(function (data, status, header, config) {
                 $scope.ResponseDetails = "Data: " + data +
